ecdd963c2e0743c376581975106820e270492573
kabosnn <anagynandor@gmail.com>
2024-05-21 11:13:20 +0200

    list countries and capitals

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import './style.css'
+import getCountries from './api-client/getCountries'
+
+const app = document.querySelector<HTMLDivElement>('#app')
 
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-<h1>Hello World</h1>
-  </div>
-`
+async function main() {
+  const countries = await getCountries();
+
+  const ul = document.createElement("ul")
+  ul.className = "countries-capitals-list"
+
+  for (const country of countries) {
+    const li = document.createElement("li")
+    const p = document.createElement("p")
+    li.innerText = country.name.common
+    p.innerText = country.capitals[0]
+
+    li.append(p)
+    ul.append(li)
+  }
+app.append(ul)
+}
+main()
