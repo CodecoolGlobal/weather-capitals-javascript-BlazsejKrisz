a9d1b894c9cd1e828ae69ac0d65464e3c3f5a71c
BlazsejKrisz <b.krisz4@gmail.com>
2024-05-23 12:06:04 +0200

    complete weather task

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,50 @@
-import './style.css'
-import getCountries from './api-client/getCountries'
-import getWeather from './api-client/weather';
+import "./style.css";
+import getCountries from "./api-client/getCountries";
+import getWeather from "./api-client/weather";
 
-const app = document.querySelector<HTMLDivElement>('#app')
-
-async function convertString(inputString: string): Promise<string> {
- 
-  return inputString.replace(/[^\w\s]/gi, '');
-}
-
-
+const app = document.querySelector<HTMLDivElement>("#app");
 
 async function main() {
   const countries = await getCountries();
 
-  const ul = document.createElement("ul")
-  ul.className = "countries-capitals-list"
+  const ul = document.createElement("ul");
+  ul.className = "countries-capitals-list";
 
   for (const country of countries) {
-    let capitals = await convertString(country.capitals[0])
+    let capitals = country.capitals[0];
 
-    const li = document.createElement("li")
-    const p = document.createElement("p")
-    li.innerText = country.name.common
-    p.innerText = country.capitals[0]
-    const p2 = document.createElement("p")
-    if (capitals) {
-    p2.innerText = await getWeather(capitals)
-    } 
-    li.append(p)
-    ul.append(li)
-    p.append(p2);
+    const li = document.createElement("li");
+    li.innerText = country.name.common;
+
+    const p = document.createElement("p");
+    p.innerText = country.capitals[0];
+    
+    p.addEventListener("click", async () => {
+      const existingP2 = p.querySelector("p.capitalWeather");
+      if (existingP2) {
+        p.removeChild(existingP2);
+      } else {
+        const weather = await getWeather(capitals);
+        const p2 = document.createElement("p");
+        if (capitals) {
+          p2.innerText = weather.current.temp_c;
+          p2.className = "capitalWeather";
+        } else {
+          p2.innerText = "No capital detected";
+        }
+        const weatherText = document.createElement("div")
+        weatherText.innerText = weather.current.condition.text;
+        const icon = document.createElement("img")
+        icon.src = weather.current.condition.icon;
+        p2.append(icon);
+        p2.append(weatherText);
+        p.append(p2);
+        
+      }
+    });
+    li.append(p);
+    ul.append(li);
   }
-app?.append(ul)
+  app?.append(ul);
 }
-main()
+main();
